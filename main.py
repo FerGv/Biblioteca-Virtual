f61614e68711374376ad61ec13b37c59073f17e4
Fernando García <fkngv_10@hotmail.com>
2016-11-15 01:55:48 -0600

    Diseño Index, Archivos y Comentarios

--- main.py
+++ main.py
@@ -33,16 +33,17 @@ csrf = CsrfProtect()
 def before_request():
     if 'username' not in session and request.endpoint in ['upload', 'archivos', 'uploads', 'borrar_archivo', 'comentarios', 'bienvenida', 'temas', 'crear_tema', 'logout']:
         return redirect(url_for('login'))
-    elif 'username' in session and request.endpoint in ['login', 'registro']:
+    elif 'username' in session and request.endpoint in ['index', 'login', 'registro']:
         return redirect(url_for('bienvenida'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    title = 'Index'
+    return render_template('index.html', title = title)
 
 @app.route('/bienvenida')
 def bienvenida():
-    title = 'Index'
+    title = 'Inicio'
     return render_template('bienvenida.html', title = title)
 
 @app.route('/registro', methods = ['GET', 'POST'])
@@ -60,9 +61,10 @@ def registro():
 
         session['username'] = username
         session['user_id'] = user.id
-        return redirect(url_for('index'))
+        return redirect(url_for('bienvenida'))
 
-    return render_template('create.html', form = create_form)
+    title = 'Registro'
+    return render_template('create.html', form = create_form, title = title)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -77,11 +79,12 @@ def login():
         if user is not None and user.verify_password(password):
             session['user_id'] = user.id
             session['username'] = username
-            return redirect(url_for('index'))
+            return redirect(url_for('bienvenida'))
         else:
             flash('error_message')
 
-    return render_template('login.html', form = login_form)
+    title = 'Login'
+    return render_template('login.html', form = login_form, title = title)
 
 @app.route('/logout')
 def logout():
@@ -89,7 +92,7 @@ def logout():
         session.pop('username')
         session.pop('user_id')
 
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
@@ -108,7 +111,8 @@ def upload():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('archivos'))
     else:
-        return render_template('upload.html')
+        title = 'Upload'
+        return render_template('upload.html', title = title)
 
 @app.route('/uploads/<filename>')
 def uploads(filename):
@@ -119,7 +123,8 @@ def archivos():
     mypath = '/home/fer_gv/GitHub/Biblioteca-Virtual/Archivos/'
     archivos = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
 
-    return render_template('archivos.html', archivos = archivos)
+    title = 'Biblioteca'
+    return render_template('archivos.html', archivos = archivos, title = title)
 
 @app.route('/borrar_archivo/<filename>')
 def borrar_archivo(filename):
@@ -140,14 +145,18 @@ def comentarios(id_tema = 1):
         db.session.add(comentario)
         db.session.commit()
         comment_form.comment.data = ""
-        return render_template('comentarios.html', comments = comments, form = comment_form, id_tema = id_tema, date_format = date_format, tema = tema.tema)
 
-    return render_template('comentarios.html', comments = comments, form = comment_form, id_tema = id_tema, date_format = date_format, tema = tema.tema)
+        title = 'Comentarios'
+        return render_template('comentarios.html', title = title, comments = comments, form = comment_form, id_tema = id_tema, date_format = date_format, tema = tema.tema)
+
+    title = 'Comentarios'
+    return render_template('comentarios.html', title = title, comments = comments, form = comment_form, id_tema = id_tema, date_format = date_format, tema = tema.tema)
 
 @app.route('/temas')
 def temas():
     temas = Theme.query.all()
-    return render_template('temas.html', temas = temas)
+    title = 'Foro'
+    return render_template('temas.html', temas = temas, title = title)
 
 @app.route('/crear_tema', methods = ['GET', 'POST'])
 def crear_tema():
@@ -164,7 +173,8 @@ def crear_tema():
         flash('Tema creado')
         return redirect(url_for('temas'))
 
-    return render_template('crear_tema.html', form = theme_form)
+    title = 'Crear Tema'
+    return render_template('crear_tema.html', form = theme_form, title = title)
 
 if __name__ == '__main__':
     csrf.init_app(app)
